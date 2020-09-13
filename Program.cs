157d3eada483dae0241b08459f69f1b1e085a5c7
Gediminas Krasauskas <gediminas.krasauskas@mif.stud.vu.lt>
2020-09-13 13:03:06 +0300

    update-namings

--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@ namespace AssetFinancingStudy
 {
     class Program
     {
-        private const string FILE_PATH = "C:/Users/Gediminas/source/repos/AssetFinancingStudy/DataSets/TestData.txt";
+        private const string FILE_PATH = "C:/Users/Gediminas/source/repos/gedgeds/DB-Asset-Financing-Study/DataSets/TestData.txt";
         private const string SEPARATOR = ", ";
 
         static void Main()
         {
-            var pyramid = FileReader.ReadPyramidNumbers(FILE_PATH);
+            var pyramid = PyramidFileReader.ReadPyramid(FILE_PATH);
             var pyramidManager = new PyramidManager(pyramid);
 
             pyramidManager.PreparePyramid();
---
+++ Utilities/PyramidFileReader.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+
+namespace AssetFinancingStudy
+{
+    public static class PyramidFileReader
+    {
+        private const char DELIMITER = ' ';
+
+        /* 
+         * Manages pyramid data extraction from file and returns the pyramid
+         */
+        public static int[,] ReadPyramid(string filename)
+        {
+            StreamReader r = new StreamReader(filename);
+
+            var rows = CountLines(r);
+            var pyramid = FillMatrix(rows, r);
+            r.Close();
+
+            return pyramid;
+        }
+
+        /* 
+         * Returns the amount of lines in the file
+         */
+        private static int CountLines(StreamReader r)
+        {
+            int lines = 0;
+
+            while ((_ = r.ReadLine()) != null)
+            {
+                lines++;
+            }
+
+            return lines;
+        }
+
+        /* 
+         * Reads the file and returns pyramid 
+         * structure in matrix (2D array) format
+         */
+        private static int[,] FillMatrix(int matrixSize, StreamReader r)
+        {
+            string row;
+            string[] rowElements;
+
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            r.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            int j = 0;
+            while ((row = r.ReadLine()) != null)
+            {
+                rowElements = row.Split(DELIMITER);
+                for (int i = 0; i < rowElements.Length; i++)
+                {
+                    matrix[j, i] = int.Parse(rowElements[i]);
+                }
+                j++;
+            }
+
+            return matrix;
+        }
+
+    }
+}
